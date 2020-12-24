cf6994f5ef8e4da2ddb9d0181884096ab7807f90
Carla2748 <britneyspears91@hotmail.com>
2020-12-24 08:45:41 +0000

    Aded notes

--- Assigment_2.R
+++ Assigment_2.R
@@ -1,3 +1,5 @@
+## Here I start the code creating a matrix that uses the inverse
+
 makeCacheMatrix <- function( mat = matrix() ) {
   
    inv <- NULL
@@ -6,25 +8,28 @@ makeCacheMatrix <- function( mat = matrix() ) {
     mat <<- matrix
     inv <<- NULL
   }
-  
+
+## Here I use the method get for the matrix
   get <- function() {
     mat
   }
-  
+
+  ## Here I use the method set inverse for the matrix
   setInverse <- function(inverse) {
     inv <<- inverse
   }
-
+  ## Here I use the method get inverse for the matrix
   getInverse <- function() {
     inv
   }
  
+## I set it to get a final list with all the methods used so far  
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
-
+## In this section I create the code to get the solution of the inverse of the cache
 cacheSolve <- function(x, ...) {
   mat <- x$getInverse()
 
@@ -32,10 +37,11 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(mat)
   }
-  
+## Calculate the inverse and set it  
   data <- x$get()
   mat <- solve(data) %*% data
   x$setInverse(mat)
-  
+
+  ##Return the final matrix  
   mat
 }
